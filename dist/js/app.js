d2b827f08e7341470771134aafdb150ae42d9a30
Alessandro <internicola.alessandro@gmail.com>
2021-01-26 17:26:35 +0100

    Navbar completata
    Aggiunte le utiliti class e l.e variabili tramite sass, usato vue per la generazione dei link della nav

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,7 +7,49 @@
   \********************/
 /***/ (() => {
 
-
+var root = new Vue({
+  el: "#root",
+  data: {
+    logo: "./dist/img/logo.svg",
+    navLink: [{
+      name: "Solutions",
+      subMenu: [{}]
+    }, {
+      name: "Pricing",
+      subMenu: [{}]
+    }, {
+      name: "Blog",
+      subMenu: [{}]
+    }, {
+      name: "Sign in",
+      subMenu: [{}]
+    }],
+    workFlowImgOne: [{
+      img: "./dist/img/hardcore.png",
+      text: "they are hardcore, formal and disconnected"
+    }, {
+      img: "./dist/img/nofeedback.png",
+      text: "they are hardcore, formal and disconnected"
+    }, {
+      img: "./dist/img/notsafe.png",
+      text: "they are hardcore, formal and disconnected"
+    }],
+    workFlowImgtwo: [{
+      img: "./dist/img/lightweight.png",
+      text: "it's lightweight fun and interconnected"
+    }, {
+      img: "./dist/img/feedbacksuggestions.png",
+      text: "it encurages regular feedback with contextual request"
+    }, {
+      img: "./dist/img/safe.png",
+      text: "It creates a safe environment for dialog"
+    }]
+  },
+  methods: {// topScroll() {
+    //   window.scrollTo(0, 0);
+    // },
+  }
+}); // fine
 
 /***/ }),
 
